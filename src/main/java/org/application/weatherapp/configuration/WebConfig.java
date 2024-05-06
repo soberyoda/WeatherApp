92da869e0277db54d3601541e00b11aaf4b10a6c
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-05-06 16:46:53 +0200

    cors mappings

--- src/main/java/org/application/weatherapp/configuration/WebConfig.java
+++ src/main/java/org/application/weatherapp/configuration/WebConfig.java
@@ -2,13 +2,16 @@ package org.application.weatherapp.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("CorsMappings are being applied...");
         registry.addMapping("/**")
                 .allowedOrigins("https://weatherappfrontend-gd5m.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
